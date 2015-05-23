44c038f59212dd682060042e049f74499566d6dc
Oj <Oj@Ojizzle-PC.mshome.net>
2015-05-23 12:30:50 +0100

    Added AL Sound

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,8 @@
 
 import java.awt.Font;
 import java.io.IOException;
-import java.nio.FloatBuffer;
 import java.util.Arrays;
-import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
-import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
 import org.lwjgl.input.*;
 import org.lwjgl.opengl.*;
@@ -19,6 +16,14 @@ import org.newdawn.slick.util.ResourceLoader;
 import org.newdawn.slick.opengl.*;
 import static org.newdawn.slick.opengl.renderer.SGL.GL_TEXTURE_2D;
 import static org.newdawn.slick.opengl.renderer.SGL.GL_TEXTURE_WRAP_S;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.util.Scanner;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.openal.AL;
+import org.lwjgl.openal.AL10;
+import org.lwjgl.util.WaveData;
 
 public class Game {
               
@@ -26,7 +31,34 @@ public class Game {
     
     private Texture atlas;
           
-
+ /** Buffers hold sound data. */
+  IntBuffer buffer = BufferUtils.createIntBuffer(1);
+ 
+  /** Sources are points emitting sound. */
+  IntBuffer source = BufferUtils.createIntBuffer(1);
+ 
+  /** Position of the source sound. */
+  FloatBuffer sourcePos = (FloatBuffer)BufferUtils.createFloatBuffer(3).put(new float[] { 0.0f, 0.0f, 0.0f }).rewind();
+ 
+  /** Velocity of the source sound. */
+  FloatBuffer sourceVel = (FloatBuffer)BufferUtils.createFloatBuffer(3).put(new float[] { 0.0f, 0.0f, 0.0f }).rewind();
+ 
+  /** Position of the listener. */
+  FloatBuffer listenerPos = (FloatBuffer)BufferUtils.createFloatBuffer(3).put(new float[] { 0.0f, 0.0f, 0.0f }).rewind();
+ 
+  /** Velocity of the listener. */
+  FloatBuffer listenerVel = (FloatBuffer)BufferUtils.createFloatBuffer(3).put(new float[] { 0.0f, 0.0f, 0.0f }).rewind();
+ 
+  /** Orientation of the listener. (first 3 elements are "at", second 3 are "up") */
+  FloatBuffer listenerOri = (FloatBuffer)BufferUtils.createFloatBuffer(6).put(new float[] { 0.0f, 0.0f, -1.0f,  0.0f, 1.0f, 0.0f }).rewind();
+ 
+  /**
+  * boolean LoadALData()
+  *
+  *  This function will load our sample data from the disk using the Alut
+  *  utility and send the data into OpenAL as a buffer. A source is then
+  *  also created to play that buffer.
+  * **/
     
      //move to a interface class???
 //     boolean DebugMode = false;
@@ -69,6 +101,21 @@ public class Game {
         Display.setVSyncEnabled(VSYNC); //whether hardware VSync is enabled
         Display.setFullscreen(FULLSCREEN); //whether fullscreen is enabled
 
+            try{
+      AL.create();
+    } catch (LWJGLException le) {
+      le.printStackTrace();
+      return;
+    }
+    AL10.alGetError();
+ 
+    // Load the wav data.
+    if(loadALData() == AL10.AL_FALSE) {
+      System.out.println("Error loading data.");
+      return;
+    }
+ 
+    setListenerValues();
         //create and show our display
         Display.create();
        } catch (LWJGLException e) {
@@ -174,11 +221,12 @@ public class Game {
         coord = camera.getPosition();
                 
         if (Keyboard.isKeyDown(Keyboard.KEY_W))//move forward
-            {if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))//toggle sprint
-            {movementSpeed = 0.03f;}
-            else{movementSpeed = 0.01f;}
-
-            camera.walkForward(movementSpeed*delta);}
+              {AL10.alSourcePlay(source.get(0));
+                if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))//toggle sprint
+                {movementSpeed = 0.03f;}
+                else{movementSpeed = 0.01f;}
+                camera.walkForward(movementSpeed*delta);}
+        else{AL10.alSourceStop(source.get(0));}
         
         if (Keyboard.isKeyDown(Keyboard.KEY_S))//move backwards
             {if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))//toggle sprint
@@ -400,6 +448,111 @@ GL11.glEnable(GL11.GL_LIGHTING);
               return output;
        
    }
-  
+   
+
+ 
+
+  int loadALData() {
+    // Load wav data into a buffer.
+    AL10.alGenBuffers(buffer);
+ 
+    if(AL10.alGetError() != AL10.AL_NO_ERROR)
+      return AL10.AL_FALSE;
+ 
+    //Loads the wave file from your file system
+    /*java.io.FileInputStream fin = null;
+    try {
+      fin = new java.io.FileInputStream("FancyPants.wav");
+    } catch (java.io.FileNotFoundException ex) {
+      ex.printStackTrace();
+      return AL10.AL_FALSE;
     }
+    WaveData waveFile = WaveData.create(fin);
+    try {
+      fin.close();
+    } catch (java.io.IOException ex) {
+    }*/
+ 
+    //Loads the wave file from this class's package in your classpath
+    WaveData waveFile = WaveData.create("WalkingGravel.wav");
+ 
+    AL10.alBufferData(buffer.get(0), waveFile.format, waveFile.data, waveFile.samplerate);
+    waveFile.dispose();
+ 
+    // Bind the buffer with the source.
+    AL10.alGenSources(source);
+ 
+    if (AL10.alGetError() != AL10.AL_NO_ERROR)
+      return AL10.AL_FALSE;
+ 
+    AL10.alSourcei(source.get(0), AL10.AL_BUFFER,   buffer.get(0) );
+    AL10.alSourcef(source.get(0), AL10.AL_PITCH,    1.0f          );
+    AL10.alSourcef(source.get(0), AL10.AL_GAIN,     1.0f          );
+    AL10.alSource (source.get(0), AL10.AL_POSITION, sourcePos     );
+    AL10.alSource (source.get(0), AL10.AL_VELOCITY, sourceVel     );
+ 
+    // Do another error check and return.
+    if (AL10.alGetError() == AL10.AL_NO_ERROR)
+      return AL10.AL_TRUE;
+ 
+    return AL10.AL_FALSE;
+  }
+ 
+  /**
+   * void setListenerValues()
+   *
+   *  We already defined certain values for the Listener, but we need
+   *  to tell OpenAL to use that data. This function does just that.
+   */
+  void setListenerValues() {
+    AL10.alListener(AL10.AL_POSITION,    listenerPos);
+    AL10.alListener(AL10.AL_VELOCITY,    listenerVel);
+    AL10.alListener(AL10.AL_ORIENTATION, listenerOri);
+  }
+ 
+  /**
+   * void killALData()
+   *
+   *  We have allocated memory for our buffers and sources which needs
+   *  to be returned to the system. This function frees that memory.
+   */
+  void killALData() {
+    AL10.alDeleteSources(source);
+    AL10.alDeleteBuffers(buffer);
+  }
+ 
+ 
+  public void execute() {
+    // Initialize OpenAL and clear the error bit.
+
+ 
+    // Loop.
+
+    char c = ' ';
+    Scanner stdin = new Scanner(System.in);
+    while(c != 'q') {
+      try {
+        System.out.print("Input: ");
+        c = (char)stdin.nextLine().charAt(0);
+      } catch (Exception ex) {
+        c = 'q';
+      }
+ 
+      switch(c) {
+        // Pressing 'p' will begin playing the sample.
+        case 'p': AL10.alSourcePlay(source.get(0)); break;
+ 
+        // Pressing 's' will stop the sample from playing.
+        case's': AL10.alSourceStop(source.get(0)); break;
+ 
+        // Pressing 'h' will pause the sample.
+        case 'h': AL10.alSourcePause(source.get(0)); break;
+      };
+    }
+    killALData();
+    AL.destroy();
+  }
+}
+  
+    
 
